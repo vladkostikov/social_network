a5f6faf78f31fc641a5b8b33502598a81be94239
Vlad Kostikov <vladkostikovru@gmail.com>
2023-02-08 12:31:43 +0300

    Добавил шаблоны страниц

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/index'
+  get 'posts/:id', to: 'posts#show'
   root to: 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
