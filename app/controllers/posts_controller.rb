04940fbd803d4ce0ea11e4e5d9658b6d8a92dc51
Vlad Kostikov <vladkostikovru@gmail.com>
2023-02-08 17:34:23 +0300

    Добавил возможность изменять пост

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,11 +11,26 @@ class PostsController < ApplicationController
     @topics = Topic.all
   end
 
+  def edit
+    @topics = Topic.all
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.new(post_params)
     @post.save
 
     redirect_to '/posts/'.concat(@post.id.to_s)
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to '/posts/'.concat(@post.id.to_s)
+    else
+      render 'edit'
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'posts/new', to: 'posts#new'
   get 'posts/:id', to: 'posts#show'
   post 'posts/create', to: 'posts#create'
+  get 'posts/:id/edit', to: 'posts#edit'
+  post 'posts/:id', to: 'posts#update'
+
 
   root to: 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
