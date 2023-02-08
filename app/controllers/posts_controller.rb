bb24253797e38c761f86fa8fb5df26afdbd1602e
Vlad Kostikov <vladkostikovru@gmail.com>
2023-02-08 16:20:49 +0300

    Добавил страницу для создания новой публикации

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,24 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+    @topics = Topic.all
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to '/posts/'.concat(@post.id.to_s)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :topic_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'posts/index'
+  get 'posts/new', to: 'posts#new'
   get 'posts/:id', to: 'posts#show'
+  post 'posts/create', to: 'posts#create'
+
   root to: 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
