5accdb6cf1fed3b39ba15a38ac21c3b9cec762a3
Vlad Kostikov <vladkostikovru@gmail.com>
2023-02-09 08:37:21 +0300

    Переписал маршруты

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index'
+  resources :posts, except: :destroy
   get 'posts/:id', to: 'posts#show', constraints: { id: /\d+/ }
   get 'posts/:topic', to: 'posts#topic'
-
-  post 'posts', to: 'posts#create'
-  get 'posts/:id/edit', to: 'posts#edit'
-  post 'posts/:id', to: 'posts#update'
-
-
-  root to: 'posts#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
